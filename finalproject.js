2b222bb1f92bb7bc5532438adb72c551a11294e3
quiana2016 <williamsquiana@ymail.com>
2016-05-26 19:26:49 +0000

    https://github.com/Missfreeman/QuianaWilliams.github.io.gi

---
+++ finalproject.js
@@ -0,0 +1,9 @@
+var rsvp = prompt("Whould you like to rsvp");
+var name = prompt("Please enter your name");
+var address = prompt("What is your email Address");
+
+document.write(name);
+document.write(address);
+document.write(rsvp);
+
+
---
+++ project.js
@@ -0,0 +1,12 @@
+var rsvp = prompt("Would you like to rsvp");
+var name = prompt("Please enter your name");
+var address = prompt("What is your email Address");
+
+
+document.write(rsvp);
+<br/>;
+document.write(name);
+<br/>;
+document.write(address);
+<br/>;
+
